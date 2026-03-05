0fccf4863d0f4cd225efc3691d95bbbb31bfc8fc
jihye <hanjihye68@ewhain.net>
2026-03-05 21:48:00 +0900

    ìbackjoon2438

---
+++ src/BackJoon2438.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BackJoon2438 {
+    public static void main(String[] args) {
+        //첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        for(int i = 1; i <= a; i++){
+            for (int j = 1; j <= i; j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
